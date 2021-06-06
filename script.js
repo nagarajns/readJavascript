1f44412938a8c07599f0426806003e1c2f88d5cb
nagesh <nagesh@revos.in>
2021-06-06 12:58:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function loadXMLDoc(file) {
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+        xhttp.open("GET",file, false);
+        xhttp.send(null);
+        return xhttp.responseXML;
+}
+
+function doSubmit(){
+
+ xmlDoc = loadXMLDoc("colors.xml");
+ var input = document.getElementById("fname").value;
+ var str = xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue;
+ var res = str.replace(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue,input);
+ document.getElementById("color").innerHTML=res;
+ console.log(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue); 
+ str.documentElement.replace(input);
+
+}
+
+ 
