1990dc94d348a111eb6d80ba2f7b58d86bb01881
Nagaraja Sabhahith <nagarajsabhahith@gmail.com>
2021-06-06 13:18:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,19 @@ function doSubmit(){
 
  xmlDoc = loadXMLDoc("colors.xml");
  var input = document.getElementById("fname").value;
- var str = xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue;
- var res = str.replace(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue,input);
- document.getElementById("color").innerHTML=res;
- console.log(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue); 
- str.documentElement.replace(input);
+//  var str = xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue;
+//  var res = str.replace(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue,input);
+//  document.getElementById("color").innerHTML=res;
+//  console.log(xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue); 
+//  str.documentElement.replace(input);
+    
+  xmlDoc.getElementsByTagName("color")[0].childNodes[0].nodeValue = input;
+    
+  var blob = new Blob([new XMLSerializer().serializeToString(xmlDoc.documentElement)], {
+                type: "text/xml"
+            });
+
+            saveAs(blob, "test.xml");
 
 }
 
